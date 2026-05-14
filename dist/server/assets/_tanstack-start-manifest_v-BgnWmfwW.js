e4374794f1ca015c2d5e282513023a6d3aad466e
Vinayak2356 <209646365+Vinayak2356@users.noreply.github.com>
2026-05-14 22:12:23 +0530

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-BgnWmfwW.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/vinayak singh/Downloads/New folder (12)/exam-formatter-pro-BEST SCHOOL PROJECT REOLVE BIGGER AND DAILY PROBLEM/src/routes/__root.tsx", children: ["/", "/api/public/generate-exam", "/api/public/generate-hhw"], assets: void 0, preloads: ["/assets/index-CEVRHAdk.js"] }, "/": { filePath: "C:/Users/vinayak singh/Downloads/New folder (12)/exam-formatter-pro-BEST SCHOOL PROJECT REOLVE BIGGER AND DAILY PROBLEM/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CmlgEkBD.js"] } }, clientEntry: "/assets/index-CEVRHAdk.js" });
+export {
+  tsrStartManifest
+};
---
+++ dist/server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-B3WLJ7nS.js";
+import "node:events";
+export {
+  w as default
+};
